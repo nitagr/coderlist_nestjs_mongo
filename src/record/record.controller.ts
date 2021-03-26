f9caba365c38271bb96d645fa182049a64f2907d
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-27 01:01:37 +0530

    feat: API for adding a record added
    API for saving a record to DB added with all required fields

--- src/record/record.controller.ts
+++ src/record/record.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Res, HttpStatus, Post, Body } from '@nestjs/common';
+import { RecordService } from './record.service';
+import { Record } from './interface/record.interface';
+import { AddRecordDto } from './dto/record.dto';
 
 @Controller('record')
-export class RecordController {}
+export class RecordController {
+  constructor(private recordService: RecordService) {}
+
+  // API for adding a record of user
+  @Post()
+  async addRecord(@Res() res, @Body() addRecordDto: AddRecordDto) {
+    const record: Record = await this.recordService.addRecord(addRecordDto);
+    return res.status(HttpStatus.CREATED).json({
+      status: 201,
+      message: 'success',
+      data: record,
+    });
+  }
+}
--- src/record/record.service.ts
+++ src/record/record.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Record } from './interface/record.interface';
+import { AddRecordDto } from './dto/record.dto';
 
 @Injectable()
-export class RecordService {}
+export class RecordService {
+  constructor(
+    @InjectModel('Record') private readonly recordModel: Model<Record>,
+  ) {}
+
+  async addRecord(addRecordDto: AddRecordDto): Promise<Record> {
+    const record: Record = await new this.recordModel(addRecordDto);
+    return record.save();
+  }
+}
