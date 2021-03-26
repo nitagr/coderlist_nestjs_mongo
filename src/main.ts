96ef53e7163dc155dca1e9b4b651c044de8c7ff5
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-26 21:23:15 +0530

    feat: cors enabled and PORT changed
    Cors enabled and PORT for backend app changed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(PORT);
 }
 bootstrap();
