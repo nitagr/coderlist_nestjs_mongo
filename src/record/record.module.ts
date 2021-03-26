e615713604e3694df6415789281ce97f9efcea1a
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-26 23:37:10 +0530

    feat: Record controller added
    Controller for handling record routes created

--- src/record/record.module.ts
+++ src/record/record.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RecordService } from './record.service';
+import { RecordController } from './record.controller';
 
 @Module({
-  providers: [RecordService]
+  providers: [RecordService],
+  controllers: [RecordController]
 })
 export class RecordModule {}
