fdfc34650029ff0e489cce320b58a35986069276
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-27 00:02:05 +0530

    feat: Database connection setup
    Database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { RecordModule } from './record/record.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [RecordModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    RecordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/record/record.module.ts
+++ src/record/record.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RecordService } from './record.service';
 import { RecordController } from './record.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RecordSchema } from './record.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Record', schema: RecordSchema }]),
+  ],
   providers: [RecordService],
-  controllers: [RecordController]
+  controllers: [RecordController],
+  exports: [MongooseModule],
 })
 export class RecordModule {}
