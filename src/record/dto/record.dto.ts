746014ecc2b83122f8575a8aac866b1b73bcad26
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-27 13:58:53 +0530

    feat: Validation in Dto added
    Request validation added with class-validator package in AddRecordDto

--- src/record/dto/record.dto.ts
+++ src/record/dto/record.dto.ts
@@ -1,8 +1,30 @@
+import { IsEmail, IsNotEmpty, Length, IsAlphanumeric } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class AddRecordDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   gender: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(10, 13)
   mobile: string;
+
+  @IsNotEmpty()
   technologies: string;
+
+  @IsNotEmpty()
   profile: string;
+}
+
+export class UpdateRecordDto extends PartialType(AddRecordDto) {}
+
+export class FindOneParams {
+  @IsAlphanumeric()
+  id: string;
 }
