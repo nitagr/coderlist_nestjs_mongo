665631c81d0638e10ae6d5b3f5297e73fc48d5f0
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-04-15 00:05:37 +0530

    feat: Test case added for record update API
    Test case added for user update record by its id.

--- src/record/record.service.spec.ts
+++ src/record/record.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RecordService } from './record.service';
 import { RecordModule } from './record.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AddRecordDto } from './dto/record.dto';
+import { AddRecordDto, UpdateRecordDto } from './dto/record.dto';
 import { RecordSchema } from './record.schema';
 import 'dotenv/config';
 
@@ -11,13 +11,23 @@ describe('RecordService', () => {
   const recordDto: AddRecordDto = {
     name: 'Nitish',
     gender: 'male',
-    email: 'service5@eazydiner.com',
+    email: 'service6@eazydiner.com',
     mobile: '+911233123121',
     technologies: '| C++ | Javascript | ',
     profile: 'pic5.jpeg',
   };
 
-  const idForRecordDeletion = '60772dec2d5889332cc8cc59';
+  const updateRecordDto: UpdateRecordDto = {
+    name: 'Nitish updated 11',
+    gender: 'male updated 11',
+    email: 'service10upd@eazydiner.com',
+    mobile: '+911233123121',
+    technologies: '| C++ | Javascript | ',
+    profile: 'pic5upd11.jpeg',
+  };
+
+  const idForRecordDeletion = '60772d3adb718931c7522059';
+  const idForRecordUpdate = '60772e9370025b33ecadf722';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -61,6 +71,7 @@ describe('RecordService', () => {
       expect(user.name).toEqual(recordDto.name);
       expect(user.gender).toEqual(recordDto.gender);
       expect(user.email).toEqual(recordDto.email);
+      expect(user.technologies).toEqual(recordDto.technologies);
       expect(user.mobile).toEqual(recordDto.mobile);
       expect(user.profile).toEqual(recordDto.profile);
     });
@@ -74,5 +85,21 @@ describe('RecordService', () => {
   it('should delete a Record with its ID', async () => {
     const res = await service.deleteRecordById(idForRecordDeletion);
     expect(res).not.toBe(null);
+  });
+
+  it('should update a record with its ID', async () => {
+    const user = await service.updateRecordById(
+      idForRecordUpdate,
+      updateRecordDto,
+    );
+
+    const updatedUser = await service.findRecordById(idForRecordUpdate);
+    expect(updatedUser).not.toBeNull();
+    expect(updatedUser.name).toEqual(updateRecordDto.name);
+    expect(updatedUser.gender).toEqual(updateRecordDto.gender);
+    expect(updatedUser.email).toEqual(updateRecordDto.email);
+    expect(updatedUser.mobile).toEqual(updateRecordDto.mobile);
+    expect(updatedUser.profile).toEqual(updateRecordDto.profile);
+    expect(updatedUser.technologies).toEqual(updateRecordDto.technologies);
   });
 });
