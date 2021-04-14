a19609fa70caedde8aba54b4c5b850d15c6a2781
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-04-14 17:28:06 +0530

    fix: auto generated modified for mongoose service
    mongoose module import fixed in auto generated test files.

--- src/record/record.controller.spec.ts
+++ src/record/record.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordController } from './record.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RecordModule } from './record.module';
+import 'dotenv/config';
 
 describe('RecordController', () => {
   let controller: RecordController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [RecordController],
+      imports: [
+        MongooseModule.forRoot(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+        }),
+        RecordModule,
+      ],
     }).compile();
 
     controller = module.get<RecordController>(RecordController);
--- src/record/record.service.spec.ts
+++ src/record/record.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordService } from './record.service';
+import { RecordModule } from './record.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 describe('RecordService', () => {
   let service: RecordService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecordService],
+      imports: [
+        MongooseModule.forRoot(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+        }),
+        RecordModule,
+      ],
     }).compile();
 
     service = module.get<RecordService>(RecordService);
